b2ffb8e0cf2f28f3e7c58b19124b41a91de64ea6
bferrette <brunoferrette@gmail.com>
2023-03-16 09:59:14 +0100

    Delete repeatmodeler.sh

--- repeatmodeler.sh
+++
@@ -1,6 +0,0 @@
-#Variable
-masked_asm=../repeatmasker_step1/sailfish_assembly_genbank.fasta.masked
-#Build database
-BuildDatabase -engine 'ncbi' -name sailfish_assembly.masked.db ${masked_asm}
-#RepeatModeler
-~/software/RepeatModeler-2.0.1/RepeatModeler -engine 'ncbi' -pa 32 -database sailfish_assembly.masked.db -LTRStruct > repeatmodeler.log
