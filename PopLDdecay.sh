ddba42f67b3e4ae2e3e0060e15b316c9e965e7a6
bferrette <brunoferrette@gmail.com>
2023-03-16 09:34:34 +0100

    Delete PopLDdecay.sh

--- PopLDdecay.sh
+++
@@ -1,4 +0,0 @@
-# PopLDdecay - https://github.com/BGI-shenzhen/PopLDdecay
-#
-# Description: a fast and effective tool for linkage disequilibrium decay analysis based on variant call format(VCF) files
-
