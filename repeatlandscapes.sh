19db52bb3c5eaf547e454d2070c10a92686f243d
bferrette <brunoferrette@gmail.com>
2023-03-16 09:59:34 +0100

    Delete repeatlandscapes.sh

--- repeatlandscapes.sh
+++
@@ -1,5 +0,0 @@
-# repeatlandscapes - https://littlebioinformatician.wordpress.com/scripts/repeat-landscape/
-
-perl /home/bferrette/anaconda3/envs/RMenv/share/RepeatMasker/util/calcDivergenceFromAlign.pl -s sailfish.divsum /home/bferrette/sailfish_cl/assembly_cl/repeatmasker_step2/sailfish_CL_gap_closed_2.fasta.masked.cat.gz
-
-perl /home/bferrette/anaconda3/envs/RMenv/share/RepeatMasker/util/createRepeatLandscape.pl -g 650000000 -div sailfish.divsum > sailfish.landscape.html
