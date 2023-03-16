0cd2191057a43c7e39878fabaded2e07235f486b
bferrette <brunoferrette@gmail.com>
2023-03-16 09:59:26 +0100

    Delete repeatmasker_step1.sh

--- repeatmasker_step1.sh
+++
@@ -1,9 +0,0 @@
-# Variables
-asm=~/sailfish/assembly/sailfish_assembly_genbank.fasta
-dir=~/sailfish/repeatmasker_step1
-
-# activate conda environment
-conda activate RMenv
-
-# RepestMasker
-RepeatMasker -s -engine 'ncbi' -pa 32 -species 'actinopterygii' -dir ${dir} -gff ${asm} > ${dir}/repeatmasker_step1.log
